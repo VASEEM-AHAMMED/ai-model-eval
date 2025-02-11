f8d911cc323535a17b7b4e50bb0e57044ee4a37c
Vaseem Ahammed Shaik <vaseem@Vaseems-MacBook-Air.local>
2025-02-11 01:17:18 -0600

    Add test files for CI/CD

---
+++ backend/tests/test_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from app.main import app  # Import the FastAPI app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, World!"}
